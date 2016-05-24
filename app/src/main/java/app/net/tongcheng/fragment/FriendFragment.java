ec7f55c018dba7565993d486fe2bf818e718a880
76594 <765947965@qq.com>
2016-05-24 22:31:39 +0800

    首页

--- app/src/main/java/app/net/tongcheng/fragment/FriendFragment.java
+++ app/src/main/java/app/net/tongcheng/fragment/FriendFragment.java
@@ -3,11 +3,16 @@ package app.net.tongcheng.fragment;
 import android.os.Bundle;
 import android.os.Message;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import app.net.tongcheng.R;
+import app.net.tongcheng.TCApplication;
+import app.net.tongcheng.util.ViewHolder;
 
 /**
  * @author: xiewenliang
@@ -16,8 +21,10 @@ import app.net.tongcheng.R;
  * @Copyright: Copyright (c) 2016 Tuandai Inc. All rights reserved.
  * @date: 2016/4/16 15:53
  */
-public class FriendFragment extends BaseFragment {
-
+public class FriendFragment extends BaseFragment implements View.OnClickListener, SwipeRefreshLayout.OnRefreshListener {
+    private ViewHolder mViewHolder;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private RecyclerView mRecyclerView;
 
     @Nullable
     @Override
@@ -28,6 +35,16 @@ public class FriendFragment extends BaseFragment {
     }
 
     private void initView(View view) {
+        mViewHolder = new ViewHolder(view, this);
+        mViewHolder.setText(R.id.tv_title, "好友");
+        mViewHolder.setVisibility(R.id.bt_close, View.GONE);
+        mSwipeRefreshLayout = mViewHolder.getView(R.id.mSwipeRefreshLayout);
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.refurush_color);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+        mRecyclerView = mViewHolder.getView(R.id.mRecyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(TCApplication.mContext);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
     }
 
     @Override
@@ -37,6 +54,16 @@ public class FriendFragment extends BaseFragment {
 
     @Override
     public void mHandDoSomeThing(Message msg) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onRefresh() {
 
     }
 }
