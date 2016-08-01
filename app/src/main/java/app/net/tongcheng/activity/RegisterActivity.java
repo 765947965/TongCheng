f0dc42f0af084756abeaf8e40abc593fd359235f
76594 <765947965@qq.com>
2016-08-01 20:23:00 +0800

    限制输入邀请码才能注册

--- app/src/main/java/app/net/tongcheng/activity/RegisterActivity.java
+++ app/src/main/java/app/net/tongcheng/activity/RegisterActivity.java
@@ -130,13 +130,13 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
                     ToastUtil.showToast("请先阅读并同意用户协议!");
                     return;
                 }
-                phone = et_phone.getText().toString();
                 invite_code = et_invite_code.getText().toString();
-                if (!TextUtils.isEmpty(invite_code)) {
-                    mOtherBusiness.registerInviteflagBusiness(APPCationStation.CHECK, "查询邀请码...", et_invite_code.getText().toString());
-                } else {
-                    sendAouthCode();
+                if (TextUtils.isEmpty(invite_code)) {
+                    ToastUtil.showToast("请输入邀请码!");
+                    return;
                 }
+                phone = et_phone.getText().toString();
+                mOtherBusiness.registerInviteflagBusiness(APPCationStation.CHECK, "查询邀请码...", et_invite_code.getText().toString());
                 break;
         }
     }
