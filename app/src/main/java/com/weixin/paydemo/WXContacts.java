387c536c9da8c3e6dfab9dbdefafb25f2db25de8
谢文良 <765947965@qq.com>
2016-09-10 16:36:34 +0800

    微信充值

--- app/src/main/java/com/weixin/paydemo/WXContacts.java
+++ app/src/main/java/com/weixin/paydemo/WXContacts.java
@@ -6,9 +6,9 @@ public class WXContacts {
 	// android:scheme="wxb4ba3c02aa476ea1"/>为新设置的appid
 	public static final String APP_ID = "wxe0a347a5f14d308c";
 	// 商户号
-	public static final String MCH_ID = "1252743001";
+	public static final String MCH_ID = "1386103902";
 	// API密钥，在商户平台设置
-	public static final String API_KEY = "aixincallfiona20158899aixincally";
+	public static final String API_KEY = "zhejiangzesheng20150908888tcshop";
 	// 回调地址
 	public static final String NOTIFY_URL = "http://121.40.35.3/test";
 
