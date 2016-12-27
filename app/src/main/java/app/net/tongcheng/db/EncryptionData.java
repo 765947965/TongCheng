461376ee1927194ed74facf640dc59d48bfcd4db
谢文良 <765947965@qq.com>
2016-12-27 17:49:05 +0800

    3DES加密

--- app/src/main/java/app/net/tongcheng/db/EncryptionData.java
+++ app/src/main/java/app/net/tongcheng/db/EncryptionData.java
@@ -27,7 +27,7 @@ public class EncryptionData {
     // 向量
     private final static String iv = "20161227";
 
-    private static byte[] keyBytes;
+    private static Signature sig;
 
     /**
      * 3DES加密
@@ -41,7 +41,7 @@ public class EncryptionData {
             if (TextUtils.isEmpty(plainText)) {
                 return "";
             }
-            DESedeKeySpec spec = new DESedeKeySpec(getKeyBytes());
+            DESedeKeySpec spec = new DESedeKeySpec(getKeyBytes(key));
             SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("desede");
             Key desKey = keyFactory.generateSecret(spec);
             Cipher cipher = Cipher.getInstance("desede/CBC/PKCS5Padding");
@@ -69,7 +69,7 @@ public class EncryptionData {
             if (!encryptText.startsWith("3DES")) {
                 return encryptText;
             }
-            DESedeKeySpec spec = new DESedeKeySpec(getKeyBytes());
+            DESedeKeySpec spec = new DESedeKeySpec(getKeyBytes(key));
             SecretKeyFactory keyfactory = SecretKeyFactory.getInstance("desede");
             Key deskey = keyfactory.generateSecret(spec);
             Cipher cipher = Cipher.getInstance("desede/CBC/PKCS5Padding");
@@ -82,23 +82,26 @@ public class EncryptionData {
         }
     }
 
-    private static byte[] getKeyBytes() {
-        if (keyBytes == null) {
+    private static byte[] getKeyBytes(String key) {
+        if (sig == null) {
             synchronized (EncryptionData.class) {
-                if (keyBytes == null) {
+                if (sig == null) {
                     try {
                         PackageManager pm = TCApplication.mContext.getPackageManager();
-                        Signature sig = pm.getPackageInfo(TCApplication.mContext.getPackageName(), PackageManager.GET_SIGNATURES).signatures[0];
-                        String md5Fingerprint = doFingerprint(sig.toByteArray(), "MD5");
-                        md5Fingerprint = md5Fingerprint.substring(0, 12) + md5Fingerprint.substring(md5Fingerprint.length() - 12);
-                        keyBytes = md5Fingerprint.getBytes();
+                        sig = pm.getPackageInfo(TCApplication.mContext.getPackageName(), PackageManager.GET_SIGNATURES).signatures[0];
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
             }
         }
-        return keyBytes;
+        try {
+            String md5Fingerprint = doFingerprint((sig.toCharsString() + key).getBytes(), "MD5");
+            md5Fingerprint = md5Fingerprint.substring(0, 12) + md5Fingerprint.substring(md5Fingerprint.length() - 12);
+            return md5Fingerprint.getBytes();
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     private static String doFingerprint(byte[] certificateBytes, String algorithm) throws Exception {
